2ea8ac75c168f3646919c0b1117bdd26633cf3de
Kenneth Escobar <kenetknd@gmail.com>
2021-11-16 02:43:42 -0500

    Registro Cuentas

--- app/src/main/java/com/riohistory/MainActivity.java
+++ app/src/main/java/com/riohistory/MainActivity.java
@@ -2,13 +2,27 @@ package com.riohistory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button opciones;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        opciones=(Button)findViewById(R.id.btnRegistrarse);
+        opciones.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,OpcionRegistro.class);
+                startActivity(i);
+            }
+        });
     }
 }
