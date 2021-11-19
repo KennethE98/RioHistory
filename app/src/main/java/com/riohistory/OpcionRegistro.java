9defb7ab8e0e0543b3e1d8e6e5d12a4739bf1369
kenethescobar <keneth.escobar@espoch.edu.ec>
2021-11-19 18:40:57 -0500

    paso a boton y activity e-mail

--- app/src/main/java/com/riohistory/OpcionRegistro.java
+++ app/src/main/java/com/riohistory/OpcionRegistro.java
@@ -2,13 +2,26 @@ package com.riohistory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OpcionRegistro extends AppCompatActivity {
+
+    Button correo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opcion_registro);
+        correo=(Button)findViewById(R.id.btnEmail);
+        correo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(OpcionRegistro.this,correo.class);
+                startActivity(i);
+            }
+        });
     }
 }
