c2f3bfd31320af03807032b8e3763ec6760e9f46
Kenneth Escobar <kenetknd@gmail.com>
2021-11-20 00:25:39 -0500

    Registro email 2.0

--- app/src/main/java/com/riohistory/correo.java
+++ app/src/main/java/com/riohistory/correo.java
@@ -3,8 +3,12 @@ package com.riohistory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class correo extends AppCompatActivity {
+    EditText passw,passw2;
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
